b015baa3ed104a2479fa0bc180fece3a9841a3d4
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2026-01-07 20:04:34 +0100

    Added app compose

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.widgets import DataTable, Footer
 
 
 class TimeApp(App):
-    pass
+    def compose(self) -> ComposeResult:
+        yield DataTable()
+        yield Footer()
 
 
 if __name__ == "__main__":
     app = TimeApp()
-    app.run
+    app.run()
